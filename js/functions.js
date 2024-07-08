faf2020148c1322ec127ded9c0e5223572a073df
MurtazinRenat <renat.m89@gmail.com>
2024-07-08 15:19:27 +0300

    Добавляет функцию пересчёта часов в минуты

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,23 @@
-const fitsLength = (text, maxLength) => text.length <= maxLength; //Функция для проверки длинны строки
+//Функция для проверки длинны строки
+const fitsLength = (text, maxLength) => text.length <= maxLength;
 
+//Функция для проверки палиндромов
 const isPalindrome = (sequence) => {
   const text = String(sequence).replaceAll(' ', '').toLowerCase();
   const reversedText = text.split('').reverse().join('');
   return text === reversedText;
-}; //Функция для проверки палиндромов
+};
 
+//Функция для извлечения цифр от 0 до 9
 const parseDigits = (sequence) => {
   const digits = String(sequence).replace(/[^0-9]+/g, '');
   return digits ? Number(digits) : NaN;
-}; //Функция для извлечения цифр от 0 до 9
+};
 
-void (fitsLength, isPalindrome, parseDigits);
+// Функция перевода часов в минуты
+const getHoursInMinutes = (time) => {
+  const [hours, minutes] = time.split(':');
+  return Number(hours) * 60 + Number(minutes);
+};
+
+void (fitsLength, isPalindrome, parseDigits, getHoursInMinutes);
