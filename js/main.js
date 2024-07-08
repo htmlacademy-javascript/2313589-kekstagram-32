f81560892a44a1bd0a73a1359a5dbbd035895a7f
MurtazinRenat <renat.m89@gmail.com>
2024-07-08 14:23:26 +0300

    Добавляет модуляцию

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+import './data.js';
+import './utilities.js';
+
+
 const getRandomItem = (items) => {
   const fraction = items.length * Math.random(); //получаем случайное значение в виде дроби
   const index = Math.floor(fraction); //округляем в меньшую сторону до целого числа
