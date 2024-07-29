5efcc7caf11a56919c4c1fbbca8b6a4261bb0de3
MurtazinRenat <renat.m89@gmail.com>
2024-07-29 18:42:16 +0300

    Добавляет модуль для просмотра фотографий в полноразмерном режиме

---
+++ js/gallery/popup.js
@@ -0,0 +1,37 @@
+import {renderComments} from './comments.js';
+
+const popup = document.querySelector('.big-picture');
+const cancelButton = document.querySelector('.big-picture__cancel');
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key.startsWith('Esc')) {
+    cancelButton.click();
+  }
+};
+
+function showPopup() {
+  popup.classList.remove('.hidden');
+  popup.scroll(0, 0);
+  document.body.classList.add('.modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+const hidePopup = () => {
+  popup.classList.add('.hidden');
+  document.body.classList.remove('.modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  //document.dispatchEvent(new CustomEvent('popupClose')); //&
+};
+
+const renderPopup = ({url, description, likes, comments}) => {
+  popup.querySelector('.big-picture__img img').src = url;
+  popup.querySelector('.big-picture__img img').alt = description;
+  popup.querySelector('.social__caption').textContent = description;
+  popup.querySelector('.likes-count').textContent = likes;
+  renderComments(comments);
+  showPopup();
+};
+
+cancelButton.addEventListener('click', () => hidePopup());
+
+export {renderPopup};
