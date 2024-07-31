261192023d1f9d4c602be8eb3af1b0ed9de12d71
MurtazinRenat <renat.m89@gmail.com>
2024-07-31 23:44:15 +0300

    Добавляет отображение окна редактирования файла при загрузке

--- js/upload/main.js
+++ js/upload/main.js
@@ -1,4 +1,10 @@
 import {showPopup} from './popup';
 import './validation.js';
 
-showPopup();
+const form = document.querySelector('.img-upload__form');
+
+form.addEventListener('change', (evt) => {
+  if (evt.target.name === 'filename') {
+    showPopup();
+  }
+});
