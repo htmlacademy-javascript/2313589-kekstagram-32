c3c6af287559a7ed2a7dc28ff1bf9e4e52211979
MurtazinRenat <renat.m89@gmail.com>
2024-07-31 23:34:00 +0300

    Добавляет проверку длинны описания

--- js/upload/validation.js
+++ js/upload/validation.js
@@ -29,3 +29,8 @@ pristine.addValidator(form.hashtags, (text) => {
   const maxHashtagLength = 20;
   return split(text).every((word) => word.length) <= maxHashtagLength;
 }, 'Слишком длинный #-тэг', 1, true);
+
+pristine.addValidator(form.description, (text) => {
+  const maxDescriptionLength = 140;
+  return text.length <= maxDescriptionLength;
+}, 'Слишком длинный комментарий', 1, true);
