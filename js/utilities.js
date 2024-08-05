e9abc29bd6f3f051acba94a426242d6ad6781374
MurtazinRenat <renat.m89@gmail.com>
2024-08-05 17:52:42 +0300

    Удаляет не используемые функции

--- js/utilities.js
+++ js/utilities.js
@@ -6,15 +6,4 @@ const request = async (url, options) => {
   return response.json();
 };
 
-const getRandomItem = (items) => {
-  const fraction = items.length * Math.random(); //получаем случайное значение в виде дроби
-  const index = Math.floor(fraction); //округляем в меньшую сторону до целого числа
-  return items[index];
-};//Функция для получения случайного элемента массива
-
-const getRandomInteger = (min, max) => {
-  const fraction = (max - min) * Math.random() + min; //получаем значение не ниже минимального
-  return Math.round(fraction); //возвращаем с округлением(включительно)
-};//Функция для получения случайного целого в диапазоне
-
-export {request, getRandomItem, getRandomInteger};
+export {request};
