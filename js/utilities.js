f87c758fc644ef9e2d2f5d02ca8f81d01625cada
MurtazinRenat <renat.m89@gmail.com>
2024-08-05 17:01:54 +0300

    Добавляет функцию отправки сетевых запросов

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,11 @@
+const request = async (url, options) => {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return response.json();
+};
+
 const getRandomItem = (items) => {
   const fraction = items.length * Math.random(); //получаем случайное значение в виде дроби
   const index = Math.floor(fraction); //округляем в меньшую сторону до целого числа
@@ -9,4 +17,4 @@ const getRandomInteger = (min, max) => {
   return Math.round(fraction); //возвращаем с округлением(включительно)
 };//Функция для получения случайного целого в диапазоне
 
-export {getRandomItem, getRandomInteger};
+export {request, getRandomItem, getRandomInteger};
