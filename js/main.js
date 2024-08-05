c9a6639e10e09b72c77d35bd348ac44f1f3be462
MurtazinRenat <renat.m89@gmail.com>
2024-08-05 18:25:20 +0300

    Добавляет отправку данных на сервер с блокировкой кнопки во время передачи данных

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 import {request} from './utilities.js';
 import {renderStatus} from './status.js';
 import {renderGallery} from './gallery/main.js';
-import './upload/main.js';
+import {setSubmitDisabled} from './upload/main.js';
 
 const baseUrl = 'https://32.javascript.htmlacademy.pro/kekstagram';
 //https://32.javascript.htmlacademy.pro/kekstagram/data.
+
+document.addEventListener('formdata', async (evt) => {
+  try {
+    setSubmitDisabled(true);
+    await request(baseUrl, {method: 'post', body: evt.formData});
+    renderStatus('success');
+  } catch {
+    renderStatus('error');
+  } finally {
+    setSubmitDisabled(false);
+  }
+});
+
 try {
   renderGallery(await request(`${baseUrl}/data`));
 } catch {
-  renderStatus('success', {autoHide: 5000});
+  renderStatus('data-error', {autoHide: 5000});
 }
--- js/upload/main.js
+++ js/upload/main.js
@@ -5,6 +5,12 @@ import {checkValidity, resetValidity} from './validation.js';
 
 const form = document.querySelector('.img-upload__form');
 const preview = document.querySelector('.img-upload__preview img');
+const submitButton = document.querySelector('.img-upload__submit');
+
+const setSubmitDisabled = (flag) => {
+  submitButton.disabled = flag;
+  submitButton.textContent = flag ? 'Сохраняю...' : 'Сохранить';
+};
 
 form.addEventListener('change', (evt) => {
   switch (evt.target.name) {
@@ -24,8 +30,9 @@ form.addEventListener('change', (evt) => {
 });
 
 form.addEventListener('submit', (evt) => {
-  if (!checkValidity()) {
-    evt.preventDefault();
+  evt.preventDefault();
+  if (checkValidity()) {
+    new FormData(form);
   }
 });
 
@@ -34,3 +41,5 @@ form.addEventListener('reset', () => {
   resetScale();
   resetValidity();
 });
+
+export {setSubmitDisabled};
