a29fffd29c3d48420c70c763d6a63578581af5f0
MurtazinRenat <renat.m89@gmail.com>
2024-08-05 17:23:17 +0300

    Добавляет закрытие сообщений по клику на кнопку или за пределами окна

--- js/status.js
+++ js/status.js
@@ -2,6 +2,11 @@ const renderStatus = (type) => {
   const template = document.querySelector(`#${type}`);
   const status = template.content.querySelector(`.${type}`).cloneNode(true);
 
+  status.addEventListener('click', (evt) => {
+    if (evt.target.matches(`.${type}, .${type}__button`)) {
+      status.remove();
+    }
+  });
   document.body.appened(status);
 };
 
