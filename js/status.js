06edbc047dae86ec50d5661d734a00d10f1ad8e8
MurtazinRenat <renat.m89@gmail.com>
2024-08-05 17:27:10 +0300

    Добавляет закрытие окна по нажатию на ESC

--- js/status.js
+++ js/status.js
@@ -2,11 +2,19 @@ const renderStatus = (type) => {
   const template = document.querySelector(`#${type}`);
   const status = template.content.querySelector(`.${type}`).cloneNode(true);
 
+  const onDocumentKeydown = (evt) => {
+    if (evt.key.startsWith('Esc')) {
+      status.click();
+    }
+  };
+
   status.addEventListener('click', (evt) => {
     if (evt.target.matches(`.${type}, .${type}__button`)) {
       status.remove();
+      document.removeEventListener('keydown', onDocumentKeydown);
     }
   });
+  document.addEventListener('keydown', onDocumentKeydown);
   document.body.appened(status);
 };
 
